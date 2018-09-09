a399866f6792af249aaf8cf241734840577112a3
Gurbaksh <mailtosunny24@gmail.com>
2018-09-09 23:28:48 +0530

    Implemented Product Api

---
+++ ProductWebApi/Controllers/CarController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductWebApi.Controllers
+{
+    public class CarController : ApiController
+    {
+        private productsEntities productsEntities = new productsEntities();
+        // GET: api/Car
+        public IEnumerable<Car> Get()
+        {
+            return productsEntities.Cars.ToList();
+        }
+        
+        // POST: api/Car
+        public void Post([FromBody]Car car)
+        {
+            productsEntities.Cars.Add(car);
+            productsEntities.SaveChanges();
+        }
+
+        // PUT: api/Car/5
+        public void Put(int id, [FromBody]string type)
+        {
+            var car = productsEntities.Cars.Find(id);
+
+            if (type.Equals("save"))
+            {
+                car.IsSaved = "true";
+            }
+            else if (type.Equals("book"))
+            {
+                car.IsBooked = "true";
+            }
+            productsEntities.SaveChanges();
+        }
+        
+    }
+}
---
+++ ProductWebApi/Controllers/HotelController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductWebApi.Controllers
+{
+    public class HotelController : ApiController
+    {
+        private productsEntities productsEntities = new productsEntities();
+
+        // GET: api/Hotel
+        public List<Hotel> Get()
+        {
+            return productsEntities.Hotels.ToList();
+        }
+        // POST: api/Hotel
+        public void Post([FromBody]Hotel hotel)
+        {
+            productsEntities.Hotels.Add(hotel);
+            productsEntities.SaveChanges();
+        }
+
+        // PUT: api/Hotel/5
+        public void Put(int id, [FromBody]string type)
+        {
+            var hotel = productsEntities.Hotels.Find(id);
+            
+            if(type.Equals("save"))
+            {
+                hotel.IsSaved = "true";
+            }
+            else if(type.Equals("book"))
+            {
+                hotel.IsBooked = "true";
+            }
+            productsEntities.SaveChanges();
+        }
+
+        
+    }
+}
